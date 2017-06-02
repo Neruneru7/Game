61c9c7d612a49ffad87602ac46e3d21f2757880b
WIN_NT\x16j002 <x16j002@chiba-fjb.ac.jp>
2017-06-02 11:54:20 +0900

    座標移動（ボタン）

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,46 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        int speed=10;
+        int movex;
+        int movey;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Top -= speed;
+            movey -= speed;
+            movex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Left -= speed;
+            movex -= speed;
+            movey = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Left += speed;
+            movex = speed;
+            movey = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Top += speed;
+            movey = speed;
+            movex = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += movex;
+            pictureBox1.Top += movey;
         }
     }
 }
